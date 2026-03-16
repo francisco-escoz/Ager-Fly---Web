d6aa9145b6201b32b7132a8b371f56b2003964d9
Francisco Escoz <franescoz@hotmail.com>
2026-03-16 19:04:51 -0300

    Modificacion del favicon.ico y de layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,10 +47,7 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/icon.png", type: "image/png", sizes: "96x96" },
-    ],
+    icon: "/favicon.ico",
   },
 
   openGraph: {
